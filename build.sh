9ec73c0d5d92b0697fcdd1215c11e08b45085fdf
Justinas Urbanavicius <justinas.urbanavicius@internations.org>
2020-10-14 18:16:59 +0300

    drop php 7.2

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-docker build -t gamesh/php:7.2 php7.2/
 docker build -t gamesh/php:7.3 php7.3/
 docker build -t gamesh/php:7.3-cli php7.3-cli/
 docker build -t gamesh/php:7.4 -t gamesh/php:latest php7.4/
