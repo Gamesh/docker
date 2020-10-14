65f49681ca9ba0f9a9b02ef31bc170a1254f9f41
Justinas Urbanavicius <justinas.urbanavicius@internations.org>
2020-10-14 18:01:45 +0300

    added php7.3-cli

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 
 docker build -t gamesh/php:7.2 php7.2/
 docker build -t gamesh/php:7.3 php7.3/
+docker build -t gamesh/php:7.3-cli php7.3-cli/
 docker build -t gamesh/php:7.4 -t gamesh/php:latest php7.4/
