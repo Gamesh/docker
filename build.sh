4d35624ed6186c2d0e7d6afc8ccc5d885d6b557e
Justinas Urbanavicius <justinas.urbanavicius@internations.org>
2020-10-29 15:00:01 +0200

    fixed php74-cli image creation

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 docker build -t gamesh/php:7.3 php7.3/
-docker build -t gamesh/php:7.3-cli php7.3-cli/
+docker build -t gamesh/php:7.3-cli -t gamesh/php:7.3-cli-alpine php7.3-cli/
 docker build -t gamesh/php:7.4 -t gamesh/php:latest php7.4/
+docker build -t gamesh/php:7.4-cli -t gamesh/php:7.4-cli-alpine php7.4-cli/
