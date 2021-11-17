21c3b19f47524585777b4160e17b603a202b8baa
Justinas Urbanavicius <justinas.urbanavicius@internations.org>
2021-11-17 10:22:09 +0200

    added php 8.0 apache

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-docker build -t gamesh/php:7.3 php7.3/
-docker build -t gamesh/php:7.3-cli -t gamesh/php:7.3-cli-alpine php7.3-cli/
-docker build -t gamesh/php:7.4 -t gamesh/php:latest php7.4/
-docker build -t gamesh/php:7.4-cli -t gamesh/php:7.4-cli-alpine php7.4-cli/
+export DOCKER_BUILDKIT=1
+docker build --pull -t gamesh/php:7.3 php7.3/ &&
+docker build --pull -t gamesh/php:7.3-cli -t gamesh/php:7.3-cli-alpine php7.3-cli/ &&
+docker build --pull -t gamesh/php:7.4 -t gamesh/php:latest php7.4/ &&
+docker build --pull -t gamesh/php:7.4-cli -t gamesh/php:7.4-cli-alpine php7.4-cli/ &&
+docker build --pull -t gamesh/php:8.0-cli -t gamesh/php:8.0-cli-alpine php7.4-cli/ &&
+docker build --pull -t gamesh/php:8.0 -t gamesh/php:latest php8.0/ &&
