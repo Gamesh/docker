cd4567d7d33fafdeffe94ad587cfab5cb39d4a7a
Justinas Urbanavicius <justinas.urbanavicius@internations.org>
2022-01-03 22:53:29 +0200

    added php-soap extension

--- build.sh
+++ build.sh
@@ -7,3 +7,9 @@ docker build --pull -t gamesh/php:7.4 -t gamesh/php:latest php7.4/ &&
 docker build --pull -t gamesh/php:7.4-cli -t gamesh/php:7.4-cli-alpine php7.4-cli/ &&
 docker build --pull -t gamesh/php:8.0-cli -t gamesh/php:8.0-cli-alpine php7.4-cli/ &&
 docker build --pull -t gamesh/php:8.0 -t gamesh/php:latest php8.0/ &&
+docker push gamesh/php:7.3 &&
+docker push gamesh/php:7.3-cli &&
+docker push gamesh/php:7.4 &&
+docker push gamesh/php:7.4-cli &&
+docker push gamesh/php:8.0-cli &&
+docker push gamesh/php:8.0
