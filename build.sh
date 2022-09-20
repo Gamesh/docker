877c2e74cbe3d7e737d28f6d80e932cdde98e752
Justinas Urbanavicius <justinas.urbanavicius@internations.org>
2022-09-20 09:47:58 +0300

    fixed php 8.1 image webp support

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e
 
 export DOCKER_BUILDKIT=1
 docker build --pull -t gamesh/php:7.4 php7.4/
@@ -6,4 +7,4 @@ docker build --pull -t gamesh/php:7.4-cli -t gamesh/php:7.4-cli-alpine php7.4-cl
 docker build --pull -t gamesh/php:8.0 -t gamesh/php:8.0 php8.0/
 docker build --pull -t gamesh/php:8.0-cli -t gamesh/php:8.0-cli-alpine php8.0-cli/
 docker build --pull -t gamesh/php:8.1-cli -t gamesh/php:8.1-cli-alpine -t gamesh/php:latest php8.1-cli/
-docker push --all-tags
+docker push --all-tags gamesh/php
