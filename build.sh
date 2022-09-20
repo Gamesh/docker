99d86b45570d9ed510720ddd5112c769659719cd
Justinas Urbanavicius <justinas.urbanavicius@internations.org>
2022-09-20 09:26:24 +0300

    fixed structure and added composer layer

--- build.sh
+++ build.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 
 export DOCKER_BUILDKIT=1
-docker build --pull -t gamesh/php:7.3 php7.3/ &&
-docker build --pull -t gamesh/php:7.3-cli -t gamesh/php:7.3-cli-alpine php7.3-cli/ &&
-docker build --pull -t gamesh/php:7.4 -t gamesh/php:latest php7.4/ &&
-docker build --pull -t gamesh/php:7.4-cli -t gamesh/php:7.4-cli-alpine php7.4-cli/ &&
-docker build --pull -t gamesh/php:8.0-cli -t gamesh/php:8.0-cli-alpine php7.4-cli/ &&
-docker build --pull -t gamesh/php:8.0 -t gamesh/php:latest php8.0/ &&
-docker push gamesh/php:7.3 &&
-docker push gamesh/php:7.3-cli &&
-docker push gamesh/php:7.4 &&
-docker push gamesh/php:7.4-cli &&
-docker push gamesh/php:8.0-cli &&
-docker push gamesh/php:8.0
+docker build --pull -t gamesh/php:7.4 php7.4/
+docker build --pull -t gamesh/php:7.4-cli -t gamesh/php:7.4-cli-alpine php7.4-cli/
+docker build --pull -t gamesh/php:8.0 -t gamesh/php:8.0 php8.0/
+docker build --pull -t gamesh/php:8.0-cli -t gamesh/php:8.0-cli-alpine php8.0-cli/
+docker build --pull -t gamesh/php:8.1-cli -t gamesh/php:8.1-cli-alpine -t gamesh/php:latest php8.1-cli/
+docker push --all-tags
